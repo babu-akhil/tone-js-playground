3915ffb918595dab07ca0b2deba2006c381c826f
Akhil Babu <realakhilbabu@gmail.com>
2021-06-26 15:06:32 +0530

    added a sampler track

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import BD from "./assets/sounds/bd01.wav"
 import CH from "./assets/sounds/ch02.wav"
 import SD from "./assets/sounds/sd11.wav"
 import OH from "./assets/sounds/oh02.wav"
+import Sample from "./assets/sounds/Keys 1.wav"
 
 import './App.css'
 
 
-Tone.Transport.bpm.value = 85;
+Tone.Transport.bpm.value = 100;
 
 
 function Sequencer(props){
@@ -33,6 +34,7 @@ let BDSequence;
 let SDSequence;
 let CHSequence;
 let OHSequence;
+let SampleSequence;
 
 function App(){
   Tone.Transport.bpm.value = 85;
@@ -42,6 +44,7 @@ function App(){
 
   let BDSample = new Tone.Sampler({urls: {C3:BD}, onload: () => {
     BDSequence = new Tone.Sequence((time, note) => {BDSample.triggerAttackRelease(note, "8n", time)}, bdNoteSequence).start(0);
+    BDSample.volume.value = -12;
     setBDisLoaded(true);
     }}).toDestination();
 
@@ -51,6 +54,7 @@ function App(){
 
   let SDSample = new Tone.Sampler({urls: {C3:SD}, onload: () => {
     SDSequence = new Tone.Sequence((time, note) => {SDSample.triggerAttackRelease(note, "8n", time)}, sdNoteSequence).start(0);
+    SDSample.volume.value = -12;
     setSDisLoaded(true);
     }}).toDestination();  
 
@@ -60,7 +64,7 @@ function App(){
 
   let CHSample = new Tone.Sampler({urls: {C3:CH}, onload: () => {
     CHSequence = new Tone.Sequence((time, note) => {CHSample.triggerAttackRelease(note, "8n", time)}, chNoteSequence).start(0);
-    CHSample.volume.value = -16
+    CHSample.volume.value = -30
     setCHisLoaded(true);
     }}).toDestination();      
 
@@ -70,8 +74,19 @@ function App(){
 
   let OHSample = new Tone.Sampler({urls: {C3:OH}, onload: () => {
     OHSequence = new Tone.Sequence((time, note) => {OHSample.triggerAttackRelease(note, "8n", time)}, ohNoteSequence).start(0);
-    OHSample.volume.value = -12
+    OHSample.volume.value = -30
     setOHisLoaded(true);
+    }}).toDestination();      
+
+
+  let [sampleNoteSequence, setsampleNoteSequence] = useState(Array(16).fill(null))
+
+  let [sampleisLoaded, setsampleisLoaded] = useState(false);
+
+  let sampleSample = new Tone.Sampler({urls: {C3:Sample}, onload: () => {
+    SampleSequence = new Tone.Sequence((time, note) => {sampleSample.triggerAttackRelease(note, 8, time)}, sampleNoteSequence).start(0);
+    sampleSample.volume.value = -24;
+    setsampleisLoaded(true);
     }}).toDestination();      
 
     
@@ -87,6 +102,7 @@ function App(){
     <Sequencer SequenceObject = {SDSequence} notesState = {sdNoteSequence} setnotesState = {setsdNoteSequence} name = 'SN '></Sequencer>
     <Sequencer SequenceObject = {CHSequence} notesState = {chNoteSequence} setnotesState = {setchNoteSequence} name = 'CH'></Sequencer>
     <Sequencer SequenceObject = {OHSequence} notesState = {ohNoteSequence} setnotesState = {setohNoteSequence} name = 'OH'></Sequencer>
+    <Sequencer SequenceObject = {SampleSequence} notesState = {sampleNoteSequence} setnotesState = {setsampleNoteSequence} name = 'SA'></Sequencer>
   </div>
   </div>)
 }
